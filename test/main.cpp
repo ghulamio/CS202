a221379e1866f26464c4a1299b85d644df1c437a
gahme <g_ahmeddd@yahoo.com>
2023-03-24 17:32:06 +0300

    merge method

--- test/main.cpp
+++ test/main.cpp
@@ -4,6 +4,39 @@
 
 using namespace std;
 
+BST* merge(const BST& tree1, const BST& tree2){
+    BST* tree = new BST();
+    int length1 = 0, length2 = 0;
+    // inOrderTraversal is non const, so can't be used on const objects
+    int* inOrder1 = tree1.inOrderTraversal(length1); 
+    int* inOrder2 = tree2.inOrderTraversal(length2);
+    int i = 0;
+    int j = 0;
+    while (i < length1 && j < length2){
+        if (inOrder1[i] < inOrder2[j]){
+            tree->insertItem(inOrder1[i]);
+            i++;
+        } else if (inOrder1[i] > inOrder2[j]){
+            tree->insertItem(inOrder2[j]);
+            j++;
+        } else{
+            tree->insertItem(inOrder1[i]);
+            i++;
+            j++;
+        }
+    }
+    while (i < length1){
+        tree->insertItem(inOrder1[i]);
+        i++;
+    }
+    while (j < length2){
+        tree->insertItem(inOrder2[j]);
+        j++;
+    }
+    delete[] inOrder1;
+    delete[] inOrder2;
+    return tree;
+}
 
 int main(){
     // BST* tree = new BST();
